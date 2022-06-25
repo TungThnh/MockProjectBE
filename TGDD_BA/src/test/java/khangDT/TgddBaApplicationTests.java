855b76d8c2ca8bff1621bcd850e81b86df573717
Haku0910 <kirakhang269@gmail.com>
2022-06-25 12:45:29 +0700

    CRUD
    test upload

---
+++ TGDD_BA/src/test/java/khangDT/TgddBaApplicationTests.java
@@ -0,0 +1,13 @@
+package khangDT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgddBaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
