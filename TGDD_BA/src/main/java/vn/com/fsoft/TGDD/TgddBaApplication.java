51d40023cd443bd1f17b59aa5d6145dcb93b5254
Haku0910 <kirakhang269@gmail.com>
2022-06-26 04:12:57 +0700

    Project

---
+++ TGDD_BA/src/main/java/vn/com/fsoft/TGDD/TgddBaApplication.java
@@ -0,0 +1,13 @@
+package vn.com.fsoft.TGDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgddBaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgddBaApplication.class, args);
+	}
+
+}
