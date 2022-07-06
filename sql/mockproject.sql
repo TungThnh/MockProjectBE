4fd7bbbf9861058f459bd3e480795ad376240f77
Tug <87662866+TungThnh@users.noreply.github.com>
2022-07-06 09:41:10 +0700

    Update mockproject.sql

--- sql/mockproject.sql
+++ sql/mockproject.sql
@@ -2,227 +2,225 @@ CREATE DATABASE thegioididong;
 
 CREATE TABLE product
 (
-  productID CHAR(10) NOT NULL,
-  productName VARCHAR(50) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  product_name VARCHAR(50) NOT NULL,
   price INT NOT NULL,
-  manufacturerID CHAR(10) NOT NULL,
-  categoryID INT NOT NULL,
-  productWaranty INT NOT NULL,
+  manufacturer_id CHAR(10) NOT NULL,
+  category_id INT NOT NULL,
+  product_waranty INT NOT NULL,
   image VARCHAR(300) NOT NULL,
-  interesRate INT NOT NULL,
+  interes_rate FLOAT NOT NULL,
   excusive INT NOT NULL,
-  accessoriesIncluded VARCHAR(200) NOT NULL,
-  PRIMARY KEY (productID)
+  accessories_included VARCHAR(200) NOT NULL,
+  PRIMARY KEY (product_id)
 );
 
 CREATE TABLE category
 (
-  categoryID INT NOT NULL,
-  categoryName VARCHAR(50) NOT NULL,
-  status INT NOT NULL,
-  productID CHAR(10) NOT NULL,
-  PRIMARY KEY (categoryID),
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  category_id INT auto_increment NOT NULL,
+  category_name VARCHAR(50) NOT NULL,
+  status BOOLEAN NOT NULL,
+  PRIMARY KEY (category_id)
 );
 
 CREATE TABLE manufacturer
 (
-  manufacturerID CHAR(10) NOT NULL,
-  manufacturerName VARCHAR(100) NOT NULL,
-  productID CHAR(10) NOT NULL,
-  PRIMARY KEY (manufacturerID),
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  manufacturer_id CHAR(10) NOT NULL,
+  manufacturer_name VARCHAR(100) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  PRIMARY KEY (manufacturer_id),
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE color_variant
 (
   id INT NOT NULL,
-  productID CHAR(10) NOT NULL,
-  imagePath VARCHAR(300) NOT NULL,
-  colorID CHAR(10) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  image_path VARCHAR(300) NOT NULL,
+  color_id CHAR(10) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (colorID) REFERENCES product(productID)
+  FOREIGN KEY (color_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE color
 (
-  colorName VARCHAR(50) NOT NULL,
-  colorID INT NOT NULL,
-  FOREIGN KEY (colorID) REFERENCES color_variant(id)
+  color_name VARCHAR(50) NOT NULL,
+  color_id INT NOT NULL,
+  FOREIGN KEY (color_id) REFERENCES color_variant(id)
 );
 
 CREATE TABLE product_feature
 (
   id INT NOT NULL,
-  featureVideo VARCHAR(300) NOT NULL,
-  featurePath VARCHAR(300) NOT NULL,
-  productID CHAR(10) NOT NULL,
-  PRIMARY KEY (ID),
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  feature_video VARCHAR(300) NOT NULL,
+  feature_path VARCHAR(300) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE product_camera
 (
   id INT NOT NULL,
-  imagePath VARCHAR(3000) NOT NULL,
-  productID CHAR(10) NOT NULL,
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  image_path VARCHAR(3000) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE product_specification
 (
   id INT NOT NULL,
-  productSpecification VARCHAR(300) NOT NULL,
-  productID CHAR(10) NOT NULL,
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  product_specification VARCHAR(300) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE product_article
 (
-  articleID INT NOT NULL,
-  articleURL VARCHAR(300) NOT NULL,
-  productID CHAR(10) NOT NULL,
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  article_id INT NOT NULL,
+  article_url VARCHAR(300) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE product_offer
 (
-  offerID INT NOT NULL,
-  offerDescription VARCHAR(400) NOT NULL,
-  startDate DATE NOT NULL,
-  endDate DATE NOT NULL,
-  productID CHAR(10) NOT NULL,
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  offer_id INT NOT NULL,
+  offer_description VARCHAR(400) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE product_discount
 (
-  discountID INT NOT NULL,
-  discountPrice FLOAT NOT NULL,
-  startDate DATE NOT NULL,
-  endDate DATE NOT NULL,
-  productID CHAR(10) NOT NULL,
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  discount_id INT NOT NULL,
+  discount_price FLOAT NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
-CREATE TABLE promo_code
+CREATE TABLE promocode
 (
-  promoCodeID INT NOT NULL,
-  promoCodeName CHAR(50) NOT NULL,
-  promoDescription VARCHAR(400) NOT NULL,
-  promoCondition VARCHAR(100) NOT NULL,
+  promocode_id INT NOT NULL,
+  promocode_name CHAR(50) NOT NULL,
+  promo_description VARCHAR(400) NOT NULL,
+  promo_condition VARCHAR(100) NOT NULL,
   discount FLOAT NOT NULL,
-  startDate DATE NOT NULL,
-  endDate DATE NOT NULL,
-  PRIMARY KEY (promoCodeID)
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  PRIMARY KEY (promocode_id)
 );
 
 CREATE TABLE customer
 (
-  customerID INT NOT NULL,
-  fullName VARCHAR(50) NOT NULL,
-  phoneNumber NUMERIC NOT NULL,
+  customer_id INT NOT NULL,
+  full_name VARCHAR(50) NOT NULL,
+  phone_number NUMERIC NOT NULL,
   gender CHAR(10) NOT NULL,
   address VARCHAR(200) NOT NULL,
   city VARCHAR(50) NOT NULL,
   district VARCHAR(50) NOT NULL,
   ward VARCHAR(50) NOT NULL,
   Active NUMERIC NOT NULL,
-  PRIMARY KEY (customerID)
+  PRIMARY KEY (customer_id)
 );
 
 CREATE TABLE product_introduction
 (
   id INT NOT NULL,
-  imageGallery VARCHAR(300) NOT NULL,
-  productID CHAR(10) NOT NULL,
-  PRIMARY KEY (productID),
-  FOREIGN KEY (productID) REFERENCES product(productID)
+  image_gallery VARCHAR(300) NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  PRIMARY KEY (product_id),
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE district
 (
-  districtID INT NOT NULL,
-  districName VARCHAR(100) NOT NULL,
-  PRIMARY KEY (districtID)
+  district_id INT NOT NULL,
+  distric_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (district_id)
 );
 
 CREATE TABLE ward
 (
-  wardID INT NOT NULL,
-  wardName VARCHAR(100) NOT NULL,
-  PRIMARY KEY (wardID)
+  ward_id INT NOT NULL,
+  ward_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (ward_id)
 );
 
 CREATE TABLE city
 (
-  cityID INT NOT NULL,
-  cityName VARCHAR(100) NOT NULL,
-  PRIMARY KEY (cityID)
+  city_id INT NOT NULL,
+  city_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (city_id)
 );
 
 CREATE TABLE role
 (
-  roleID CHAR(10) NOT NULL,
-  roleName VARCHAR(30) NOT NULL,
-  PRIMARY KEY (roleID)
+  role_id CHAR(10) NOT NULL,
+  role_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (role_id)
 );
 
 CREATE TABLE user
 (
-  userID CHAR(10) NOT NULL,
-  firstName VARCHAR(50) NOT NULL,
-  lastName VARCHAR(20) NOT NULL,
-  userName VARCHAR(20) NOT NULL,
+  user_id CHAR(10) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  user_name VARCHAR(20) NOT NULL,
   password VARCHAR(50) NOT NULL,
   enabled INT NOT NULL,
-  status INT NOT NULL,
+  status BOOLEAN NOT NULL,
   roleID CHAR(10) NOT NULL,
-  PRIMARY KEY (userID),
-  FOREIGN KEY (roleID) REFERENCES role(roleID)
+  PRIMARY KEY (user_id),
+  FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
 
 CREATE TABLE order_detail
 (
-  billID INT NOT NULL,
+  bill_id INT NOT NULL,
   quantity INT NOT NULL,
-  productID CHAR(10) NOT NULL,
-  promoCodeID INT NOT NULL,
-  customerID INT NOT NULL,
-  PRIMARY KEY (billID),
-  FOREIGN KEY (productID) REFERENCES product(productID),
-  FOREIGN KEY (promoCodeID) REFERENCES promo_code(promoCodeID),
-  FOREIGN KEY (customerID) REFERENCES customer(customerID)
+  product_id CHAR(10) NOT NULL,
+  promocode_id INT NOT NULL,
+  customer_id INT NOT NULL,
+  PRIMARY KEY (bill_id),
+  FOREIGN KEY (product_id) REFERENCES product(product_id),
+  FOREIGN KEY (promocode_id) REFERENCES promocode(promocode_id),
+  FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
 CREATE TABLE orders
 (
-  customerID INT NOT NULL,
-  timeCreated DATE NOT NULL,
-  billID INT NOT NULL,
-  FOREIGN KEY (billID) REFERENCES order_detail(billID)
+  customer_id INT NOT NULL,
+  time_created DATE NOT NULL,
+  bill_id INT NOT NULL,
+  FOREIGN KEY (bill_id) REFERENCES order_detail(bill_id)
 );
 
 CREATE TABLE store
 (
-  storeID INT NOT NULL,
-  storeName VARCHAR(100) NOT NULL,
+  store_id INT NOT NULL,
+  store_name VARCHAR(100) NOT NULL,
   address VARCHAR(300) NOT NULL,
-  openHours VARCHAR(200) NOT NULL,
-  districtID INT NOT NULL,
-  wardID INT NOT NULL,
-  cityID INT NOT NULL,
-  PRIMARY KEY (storeID),
-  FOREIGN KEY (districtID) REFERENCES district(districtID),
-  FOREIGN KEY (wardID) REFERENCES ward(wardID),
-  FOREIGN KEY (cityID) REFERENCES city(cityID)
+  open_hours VARCHAR(200) NOT NULL,
+  district_id INT NOT NULL,
+  ward_id INT NOT NULL,
+  city_id INT NOT NULL,
+  PRIMARY KEY (store_id),
+  FOREIGN KEY (district_id) REFERENCES district(district_id),
+  FOREIGN KEY (ward_id) REFERENCES ward(ward_id),
+  FOREIGN KEY (city_id) REFERENCES city(city_id)
 );
 
-CREATE TABLE sotre_product_in_stock
+CREATE TABLE store_product_in_stock
 (
   quantity INT NOT NULL,
-  storeID INT NOT NULL,
-  productID CHAR(10) NOT NULL,
-  FOREIGN KEY (storeID) REFERENCES store(storeID),
-  FOREIGN KEY (productID) REFERENCES product_introduction(productID)
+  store_id INT NOT NULL,
+  product_id CHAR(10) NOT NULL,
+  FOREIGN KEY (store_id) REFERENCES store(store_id),
+  FOREIGN KEY (product_id) REFERENCES product_introduction(product_id)
 );
